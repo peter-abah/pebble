6d5e12893fe6b89c41403626fd1c1cd0fe2d728e
Peter Abah <peterabah.ob@gmail.com>
2024-10-14 09:06:00 +0100

    chore: upgrade nativewind to v4

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,7 @@
 import { Stack } from "expo-router";
-import { NativeWindStyleSheet } from "nativewind";
 
-/* Makes nativewind works on web, I don't know it works
- * see: https://github.com/nativewind/nativewind/issues/470#issuecomment-1589092569
- */
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
+import "../global.css";
 
 export default function AppLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
